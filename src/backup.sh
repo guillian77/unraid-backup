2cf102ac06dbeb638cf1e4f5dd2d6d2544b4dd24
Aufrere Guillian <guillian77270@gmail.com>
2024-06-01 14:13:05 +0200

    feat: script now purge dir list

--- src/backup.sh
+++ src/backup.sh
@@ -2,49 +2,80 @@
 
 # Allow to save list of backups to target directory.
 
-# BACKUP_DIR_LIST represent a target directory to save backups.
-#                 Because backups can be saved on multiple targets.
-#                 See: config.sh
-#
-# @param [String] $1 target backup directory (remote or local server/dir).
-backup_dirs_to()
-{
-    [[ -z "$1" ]]   && error "Backup target should be specified!" && return
-    [[ ! -d "$1" ]] && error "Backup directory not found" && return
+function handle_dirs() {
+    target_dir="${1}"
 
-    target_dir="${1}/crypted"
+    [[ -z "$target_dir" ]]   && error "Backup target should be specified" && return
+    [[ ! -d "$target_dir" ]] && error "Backup directory not found" && return
 
     title "Current backup directory: ${target_dir}"
 
-    mkdir -p "${target_dir}"
+    for dir_index in "${!BACKUP_DIR_LIST[@]}"; do
+        dir_path="${BACKUP_DIR_LIST[$dir_index]}"
 
-    for dirIndex in "${!BACKUP_DIR_LIST[@]}"; do
-        dir="${BACKUP_DIR_LIST[$dirIndex]}"
+        [[ ! -d "$dir_path" ]]   && error "Directory not found under ${Yel}$dir_path${NC}" && continue
 
-        section "Backup $dirIndex"
+        backup_dirs_to "$dir_path" "$dir_index" "$target_dir"
 
-        if [ ! -d "$dir" ]; then
-            error "Directory not found under ${Yel}$dir${NC}"
-            info "SKIP, try next backup directory"
-            continue
-        fi
-
-        success "Directory found under $dir"
-
-        archiveName="$(date +'%Y-%m-%d')_${dirIndex}.tar.gz"
-
-        info "Compress && Crypt ${archiveName} to ${archiveName}.age"
-        info "Target : ${target_dir}/${archiveName}.age"
-
-        # -- Compress && Crypt
-        tar -cz ${dir} | age -r $(age-keygen -y ${SECRET_KEY_PATH}) > "${target_dir}/${archiveName}.age"
-
-        success "${archiveName}.age saved"
+        [[ $PURGE_TARGET == 1 ]] \
+            && purge_directory "$dir_path" "*tar.gz.age" \
+            || warning "Pruge currently disabled"
     done
 }
 
-backupPfsense()
+# Backup a directory to a target.
+#
+# Note: directory will be compress and encrypted.
+#
+# @param [String] $1 Path of directory to backup.
+# @param [String] $2 Name of directory to backup.
+# @param [String] $3 Target path where save backup.
+backup_dirs_to()
 {
+    dir_path="$1"
+    dir_name="$2"
+    target_dir="$3"
+
+    [[ -z "$dir_path" ]] && error "Missing directory path parameter" && return
+    [[ -z "$dir_name" ]] && error "Missing directory name parameter" && return
+    [[ -z "$target_dir" ]]   && error "Missing target directory parameter" && return
+
+    section "Backup $dir_name"
+
+    archiveName="$(date +'%Y-%m-%d')_${dir_name}.tar.gz"
+
+    info "<b>Directory</b>: ${dir_path}"
+    info "<b>Name</b>:      ${dir_name}"
+    info "<b>Size</b>:      $(du -hs $dir_path)"
+    info "<b>Archive</b>:   ${target_dir}/${archiveName}.age"
+
+    tar -cz ${dir_path} | age -r $(age-keygen -y ${SECRET_KEY_PATH}) > "${target_dir}/${archiveName}.age"
+
+    success "${archiveName}.age saved"
+}
+
+function purge_directory() {
+    target="$1"
+    pattern="$2"
+
+    [[ ! -z "$target" ]] || (error "Missing target" && exit 1)
+    [[ ! -z "$pattern" ]] || (error "Missing pattern" && exit 1)
+
+    section "Purge $dir_name"
+
+    info "<b>Retention</b>: ${DAYS_RETENTION} day(s)"
+    info "<b>Target</b>:    ${target}"
+    info "<b>Pattern</b>:   ${pattern}"
+    info "<b>Size</b>:      $(du -hs $target)"
+
+    find ${target} -name ${pattern} -mtime +${DAYS_RETENTION} -exec rm -rf {} +
+
+    [[ "$?" != 0 ]] && error "Error when trying to purge unraid old backups" && return
+
+    success "${target} purged"
+}
+
+function backup_pfsense() {
     title "Backup remote pfsense"
 
     targetBackupDir="/mnt/user/backups/Pfsense/"
@@ -58,8 +89,8 @@ backupPfsense()
 
     mkdir -p "$fullPath"
 
-    errorMessage = "Error when trying to remote copy pfsense"
+    error_message="Error when trying to remote copy pfsense"
 
-    scp -r admin@firewall.loc:/cf/conf/backup "$fullPath" || error "${errorMessage}: backup"
-    scp admin@firewall.loc:/cf/conf/config.xml "$fullPath" || error "${errorMessage}: config.xml"
+    scp -r admin@firewall.loc:/cf/conf/backup "$fullPath" || error "${error_message}: backup"
+    scp admin@firewall.loc:/cf/conf/config.xml "$fullPath" || error "${error_message}: config.xml"
 }
