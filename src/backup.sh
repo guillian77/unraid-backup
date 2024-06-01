3fbe8b846230300efca6043c9aa30d53a8a55798
Aufrere Guillian <guillian77270@gmail.com>
2024-06-01 02:19:09 +0200

    refactor: delete useless unraid function && clean

--- src/backup.sh
+++ src/backup.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 
-# Allow to backup "BACKUP_DIR_LIST" to target
+# Allow to save list of backups to target directory.
+
+# BACKUP_DIR_LIST represent a target directory to save backups.
+#                 Because backups can be saved on multiple targets.
+#                 See: config.sh
 #
 # @param [String] $1 target backup directory (remote or local server/dir).
 backup_dirs_to()
 {
-    if [ -z "$1" ];then
-        error "Backup target should be specified!"
+    [[ -z "$1" ]]   && error "Backup target should be specified!" && return
+    [[ ! -d "$1" ]] && error "Backup directory not found" && return
 
-        return
-    fi
+    target_dir="${1}/crypted"
 
-    BACKUPS_DIR="$1"
+    title "Current backup directory: ${target_dir}"
 
-    title "Backup directories to ${BACKUPS_DIR}"
-
-    if [ ! -d "$BACKUPS_DIR" ]
-    then
-        error "Backup directory not found."
-
-        info "$BACKUPS_DIR"
-
-        return
-    fi
-
-    success "Backup directory found."
-    info "$BACKUPS_DIR"
-
-    mkdir -p "$BACKUPS_DIR/crypted"
+    mkdir -p "${target_dir}"
 
     for dirIndex in "${!BACKUP_DIR_LIST[@]}"; do
         dir="${BACKUP_DIR_LIST[$dirIndex]}"
@@ -35,60 +24,42 @@ backup_dirs_to()
         section "Backup $dirIndex"
 
         if [ ! -d "$dir" ]; then
-            error "Directory not found under ${Yel}$dir${NC}."
-            info "SKIP, try next backup directory."
+            error "Directory not found under ${Yel}$dir${NC}"
+            info "SKIP, try next backup directory"
             continue
         fi
 
-        success "Directory found under $dir."
+        success "Directory found under $dir"
 
-        tempBackupDir="${BACKUPS_DIR}crypted/"
-
-        # -- Compress && Crypt
         archiveName="$(date +'%Y-%m-%d')_${dirIndex}.tar.gz"
 
-        info "Compress && Crypt ${archiveName} to ${archiveName}.age."
-        info "Target : ${tempBackupDir}${archiveName}.age"
+        info "Compress && Crypt ${archiveName} to ${archiveName}.age"
+        info "Target : ${target_dir}/${archiveName}.age"
 
-        tar -cz ${dir} | age -r $(age-keygen -y ${SECRET_KEY_PATH}) > "${tempBackupDir}${archiveName}.age"
+        # -- Compress && Crypt
+        tar -cz ${dir} | age -r $(age-keygen -y ${SECRET_KEY_PATH}) > "${target_dir}/${archiveName}.age"
+
+        success "${archiveName}.age saved"
     done
-}
-
-# Backup unraid boot configuration.
-generateUnraidFlashBackup()
-{
-    title "Backuping full unraid configuration."
-
-    targetUnraidFlashName="$(date +'%Y-%m-%d')_flash_full"
-    targetUnraidBackupDir="/mnt/user/backups/Unraid/"
-    targetUnraidFullPath="${targetUnraidBackupDir}${targetUnraidFlashName}"
-
-    if [ -d "${targetUnraidFullPath}" ]; then
-        info "Already exist, remove before: ${targetUnraidFullPath}."
-        rm -rf "${targetUnraidFullPath}"
-    fi
-
-    info "Copying unraid boot configuration."
-    cp -r "/boot" "${targetUnraidFullPath}"
-
-    success "Unraid boot configuration copied"
 }
 
 backupPfsense()
 {
-    title "Backup remote pfsense."
+    title "Backup remote pfsense"
 
     targetBackupDir="/mnt/user/backups/Pfsense/"
     pfsenseDirName="$(date +'%Y-%m-%d')_pfsense_full"
     fullPath="${targetBackupDir}${pfsenseDirName}"
 
     if [ -d "${fullPath}" ]; then
-        info "Already exist, remove before: ${fullPath}."
+        info "Already exist, remove before: ${fullPath}"
         rm -rf "${fullPath}"
     fi
 
     mkdir -p "$fullPath"
 
-    scp -r admin@firewall.loc:/cf/conf/backup "$fullPath" || true
-    scp admin@firewall.loc:/cf/conf/config.xml "$fullPath" || true
+    errorMessage = "Error when trying to remote copy pfsense"
+
+    scp -r admin@firewall.loc:/cf/conf/backup "$fullPath" || error "${errorMessage}: backup"
+    scp admin@firewall.loc:/cf/conf/config.xml "$fullPath" || error "${errorMessage}: config.xml"
 }
